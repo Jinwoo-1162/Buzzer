444995e63d949c70b6e134d96db04a8fe6f56987
Jinwoo-1162 <jinwoo1162@gmail.com>
2022-09-26 22:35:37 -0400

    useState cookie clicker tutorial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import cookie from "./cookie.png";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount((count) => count + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Cookie Clicker</h2>
+        <img id="cookie" src={cookie} onClick={incrementCount} />
+        <h2>{count}</h2>
       </header>
     </div>
   );
