8243d6e6aa5661770055e5531a4a386157f900bd
Kahee Tomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2022-10-04 19:10:41 -0400

    Create Home.js

---
+++ frontend/src/Views/Home.js
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>Hi this is a home!</h1>
+      <p> I am creating a tweeter clone website!</p>
+    </div>
+  );
+}
+
+export default Home;
