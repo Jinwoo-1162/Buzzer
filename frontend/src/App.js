3b1fd883687b35f91f88840303aa5fd9d21b2428
Kahee Tomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2022-10-04 19:25:25 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,16 @@ import TextBox from "./TextBox";
 import Home from "./Views/Home";
 import About from "./Views/About";
 import { BrowserRouter, Routes } from "react-router-dom";
-import {Route} from "react-router-dom";
+import { Route } from "react-router-dom";
+import ErrorPage from "./Views/ErrorPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/About" element={<About/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
