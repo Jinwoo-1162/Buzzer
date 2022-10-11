d448588ea48540136fdc6c490413f85f25a86306
Kahee Tomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2022-10-11 15:09:36 -0400

    Create ErrorPage.js

---
+++ frontend/src/Views/ErrorPage.js
@@ -0,0 +1,12 @@
+function ErrorPage() {
+    return (
+      <div className="App">
+        <h1>404 Page not found</h1>
+        <a href="/">Home</a>
+        <br />
+        <a href="/About">About</a>
+      </div>
+    );
+  }
+  
+  export default ErrorPage;
