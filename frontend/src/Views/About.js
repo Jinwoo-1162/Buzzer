53b1de96f59e1c239ecac9f84d367296d741c365
Kahee Tomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2022-10-11 15:09:29 -0400

    Create About.js

---
+++ frontend/src/Views/About.js
@@ -0,0 +1,13 @@
+function About() {
+    return (
+      <div className="App">
+        <h1>Hi this is about!</h1>
+        <p> Do you want to know more about us?</p>
+        <a href="/">Home</a>
+        <br />
+        <a href="/About">About</a>
+      </div>
+    );
+  }
+  
+  export default About;
