458fdad211c58d706d682c87292e09a1e8e9d10f
Kahee Tomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2022-10-11 15:09:16 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./CSS/App.css";
 import Home from "./Views/Home.js";
+import About from "./Views/About";
+import ErrorPage from "./Views/ErrorPage";
+import Messages from "./Views/Messages"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/*" element={<ErrorPage />} />
+        <Route path="/Messages" element={<Messages />} />
       </Routes>
     </BrowserRouter>
   );
