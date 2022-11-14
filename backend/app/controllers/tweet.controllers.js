d24477c03664c07205fd2c9caeec6d9246fb949f
Sherwin6180 <sherwin6180@gmail.com>
2022-11-13 19:36:30 -0500

    finish adding tweets to database by clicking tweet button

---
+++ backend/app/controllers/tweet.controllers.js
@@ -0,0 +1,43 @@
+const Tweet = require('../models/tweet.models.js');
+
+exports.find = async (req, res) => {
+    await Tweet.find({})
+    .then(tweets => {
+        res.send(tweets);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving tweets."
+        });
+    });
+};
+
+exports.findByName = async (req, res) => {
+    await Tweet.find({name: req.params.author})
+    .then(tweets => {
+        if(!tweets) {
+            return res.status(404).send({
+                message: `No entries in database Tweet not found with name ${req.params.author}`
+            });
+        }
+        res.send(tweets);
+    }).catch(err => {
+        return res.status(500).send({
+            message: `Error retrieving Tweet with name ${req.params.author}`
+        });
+    });
+};
+
+exports.addOne = async (req, res) => {
+    const newTweet = new Tweet({
+        author: req.query.author,
+        bodyText: req.query.bodyText,
+        likes: 0
+    });
+
+    newTweet.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send(err);
+    });
+};
